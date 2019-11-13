c89aefdf2d97789f5519133406edc2d6200d7a0f
joaop21 <jpsilva9898@gmail.com>
2019-11-13 18:38:02 +0000

    Add a Main for testing
    This class has the unique purpose of testing code that exists in the
    package Server

---
+++ src/main/java/Server/MainTester.java
@@ -0,0 +1,15 @@
+package Server;
+
+/**
+ * This class is for testing the current code.
+ * In the future it will be deleted.
+ * */
+public class MainTester {
+
+    public static void main(String[] args) {
+        int port = Integer.parseInt(args[0]);
+
+        new Thread(new AsynchronousProcess(port)).run();
+    }
+
+}
