73d6d58f3977de2be96d424393a33e632b1ed26f
joaop21 <jpsilva9898@gmail.com>
2019-11-15 15:42:26 +0000

    Add a Facade to Middleware package
    This Facade will be accessed from the app and from the CausalDelivery class.

---
+++ src/main/java/Server/Middleware/MiddlewareFacade.java
@@ -0,0 +1,41 @@
+package Server.Middleware;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MiddlewareFacade {
+    private CausalDelivery cd;
+    private Queue<Message> ordered_messages = new LinkedList<>();
+
+    /**
+     * Parameterized constructor that initializes an instance of MiddlewareFacade.
+     *
+     * @param p The port where the server will operate.
+     * */
+    public MiddlewareFacade(int p){
+        this.cd = new CausalDelivery(p);
+    }
+
+    /**
+     *  Method that puts an ordered message in the List to be consumed
+     *
+     * @param msg Message that will be added to the list
+     * */
+    synchronized void addMessage(Message msg){
+        this.ordered_messages.add(msg);
+        // notifies possible blocked thread
+        notify();
+    }
+
+    /**
+     * Blocking method that consumes the head of the linked list when its available
+     *
+     * @return Object Object that was exchanged in messages.
+     * */
+    public synchronized Object getMessage() throws InterruptedException {
+        while(this.ordered_messages.size() == 0)
+            wait();
+
+        return this.ordered_messages.poll().getObject();
+    }
+}
